371e732141301005140cf9e8cb35b06bc6d64361
nanecorner <daniel2000rincon@gmail.com>
2024-07-08 15:14:20 -0600

    Extensión país

--- js/main.js
+++ js/main.js
@@ -1,60 +1,69 @@
 (function() {
-  emailjs.init('rEkiW8fWxuKIjsn4b');
-})();
-
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-  event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
-  var name = document.getElementById('name').value;
-  var mail = document.getElementById('mail').value;
-  var number = '+52' + document.getElementById('number').value;
-  const NUMBER = '+524422306467'
-
-  fetch('https://thawing-mesa-75969-77a2012df85e.herokuapp.com/send-whatsapp', {
-      method: 'POST',
-      headers: {
+    emailjs.init('rEkiW8fWxuKIjsn4b');
+  })();
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const phoneInputField = document.getElementById('number');
+    const phoneInput = window.intlTelInput(phoneInputField, {
+      initialCountry: 'mx',
+      utilsScript: 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js'
+    });
+  
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+      event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
+      var name = document.getElementById('name').value;
+      var mail = document.getElementById('mail').value;
+      var number = phoneInput.getNumber();
+      const NUMBER = '+524422306467';
+  
+      fetch('https://thawing-mesa-75969-77a2012df85e.herokuapp.com/send-whatsapp', {
+        method: 'POST',
+        headers: {
           'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
+        },
+        body: JSON.stringify({
           to: NUMBER,
-          body: `Hola ${name}, pronto alguno de nuestros asesores se comunicara contigo al ${number}. Crea tu cuenta en https://nanecorner.github.io/home.html. Saludos de parte del team DiploTech.`
+          body: `Hola ${name}, pronto alguno de nuestros asesores se comunicará contigo al ${number}. Crea tu cuenta en https://nanecorner.github.io/home.html. Saludos de parte del team DiploTech.`
+        })
       })
-  })
-  .then(response => response.json())
-  .then(data => {
-      if (data.success) {
+      .then(response => response.json())
+      .then(data => {
+        if (data.success) {
           document.getElementById('confirmationMessage').style.display = 'block';
           setTimeout(function() {
-              document.getElementById('confirmationMessage').style.display = 'none';
+            document.getElementById('confirmationMessage').style.display = 'none';
           }, 10000);
-      } else {
+        } else {
           console.error('Error al enviar el mensaje de WhatsApp');
-      }
-  })
-  .catch(error => console.error('Error:', error));
-
-  emailjs.send('service_useswcj', 'template_zpib4p7', {
-      to_name: name,
-      to_mail: mail
-  }).then(function(response) {
-      console.log('Correo enviado exitosamente!', response.status, response.text);
-      document.getElementById('name').value = '';
-      document.getElementById('lastName').value = '';
-      document.getElementById('mail').value = '';
-      document.getElementById('number').value = '';
-
-      var confirmationMessage = document.getElementById('confirmationMessage');
-      confirmationMessage.innerText = '¡Gracias! Tus datos se han enviado con éxito.';
-      confirmationMessage.style.color = 'black';
-      setTimeout(function() {
+        }
+      })
+      .catch(error => console.error('Error:', error));
+  
+      emailjs.send('service_useswcj', 'template_zpib4p7', {
+        to_name: name,
+        to_mail: mail
+      }).then(function(response) {
+        console.log('Correo enviado exitosamente!', response.status, response.text);
+        document.getElementById('name').value = '';
+        document.getElementById('lastName').value = '';
+        document.getElementById('mail').value = '';
+        document.getElementById('number').value = '';
+  
+        var confirmationMessage = document.getElementById('confirmationMessage');
+        confirmationMessage.innerText = '¡Gracias! Tus datos se han enviado con éxito.';
+        confirmationMessage.style.color = 'black';
+        setTimeout(function() {
           confirmationMessage.style.color = '#EFFAF2';
-      }, 10000);
-  }, function(error) {
-      console.log('Falló el envío del correo...', error);
-      var confirmationMessage = document.getElementById('confirmationMessage');
-      confirmationMessage.innerText = 'Ha ocurrido un problema. Por favor, intenta más tarde.';
-      confirmationMessage.style.color = 'red';
-      setTimeout(function() {
+        }, 10000);
+      }, function(error) {
+        console.log('Falló el envío del correo...', error);
+        var confirmationMessage = document.getElementById('confirmationMessage');
+        confirmationMessage.innerText = 'Ha ocurrido un problema. Por favor, intenta más tarde.';
+        confirmationMessage.style.color = 'red';
+        setTimeout(function() {
           confirmationMessage.style.color = '#EFFAF2';
-      }, 10000);
+        }, 10000);
+      });
+    });
   });
-});
+  
